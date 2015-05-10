82b323e3ffa3e3d3690423ba0f4a09eba89aa606
pith <pierre.thirouin@gmail.com>
2015-05-10 20:13:21 +0200

    Add gibo, z, go workspace, files to source

--- source/10_oh_my_zsh.sh
+++ source/10_oh_my_zsh.sh
@@ -11,11 +11,9 @@ ZSH_THEME="eastwood"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git mvn osx go sbt docker brew jump)
+plugins=(git mvn osx go sbt docker brew jump tmux)
 
 source $ZSH/oh-my-zsh.sh
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
-
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
