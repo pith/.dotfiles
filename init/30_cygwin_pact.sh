53f78c3324d22b7fd88d4978a95384a87b828b7b
pith <pierre.thirouin@gmail.com>
2015-05-11 22:26:28 +0200

    Add package installer for osx and cygwin

---
+++ init/30_cygwin_pact.sh
@@ -0,0 +1,12 @@
+# Cygwin-only stuff
+is_cygwin || return 1
+
+packages=( 
+    tmux 
+    weechat 
+)
+
+e_header "Installing packages: ${packages[*]}"
+for package in "${packages[@]}"; do
+    pact install $package
+done
