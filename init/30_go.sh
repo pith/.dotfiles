142f9326a1b9097f327ea00f0b7aca7dbbb88940
Pierre T. <pierre.thirouin@gmail.com>
2015-06-09 15:51:28 +0200

    Update 30_go.sh

--- init/30_go.sh
+++ init/30_go.sh
@@ -1,7 +1,5 @@
 # Init my go workspace
-mkdir -p ~/dev/go/src
-mkdir -p ~/dev/go/bin
-mkdir -p ~/dev/go/pkg
+mkdir -p ~/dev/go/{src,bin,pkg}
 
 # Go tools for emacs
 go get -u github.com/kisielk/errcheck
