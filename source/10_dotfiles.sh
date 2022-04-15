3c56cea63db69c633aa99ff991ef9d1a7df629d0
Pierre Thirouin <pierre.thirouin@blablacar.com>
2022-04-15 15:37:39 +0200

    Remove unused config

--- source/10_dotfiles.sh
+++
@@ -1,1 +0,0 @@
-alias dot="dotfiles && . ~/.zshrc"
--- source/60_vcs.sh
+++
@@ -1,9 +0,0 @@
-# git ignore templates
-export PATH=$PATH:$DOTFILES/vendor/gibo
-. $DOTFILES/vendor/gibo/gibo-completion.zsh
-
-# git
-alias gst='git status'
-
-# svn
-alias svnrevert='svn revert --depth=infinity'
