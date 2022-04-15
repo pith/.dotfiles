66ea2e08c38a58a7624f491adbbd5576d97a65be
Pierre Thirouin <pierre.thirouin@blablacar.com>
2022-04-15 15:37:39 +0200

    Remove unused config

--- init/30_go.sh
+++
@@ -1,9 +0,0 @@
-# Init my go workspace
-mkdir -p ~/dev/go/{src,bin,pkg}
-
-# Go tools for emacs
-go get -u github.com/kisielk/errcheck
-go get -u github.com/nsf/gocode
-go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u github.com/dougm/goflymake
-go get -u github.com/golang/lint/golint
