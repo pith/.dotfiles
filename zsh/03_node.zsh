0edc62b1a275d7c0d106f21d6a95f8dabfd66cb0
Pierre Thirouin <pierre.thirouin@blablacar.com>
2025-06-22 14:16:45 +0200

    Switch from nvm to fnm as node manager

---
+++ zsh/03_node.zsh
@@ -0,0 +1,13 @@
+## NVM config
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# nvm use --lts
+
+## FNM config
+# 
+# Fast Node Manager (fnm) is a simple and fast Node.js version manager.
+# See: https://github.com/Schniz/fnm
+#
+eval "$(fnm env --use-on-cd --shell zsh)"
