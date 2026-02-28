a74bd96a666dee2fae1cb17acc1605231118a2c1
Pierre Thirouin <pierre.thirouin@doctolib.com>
2026-02-28 20:53:07 +0100

    sync config with local updates

--- zsh-config/01_path.zsh
+++ zsh-config/01_path.zsh
@@ -1,5 +1,8 @@
 export PATH=/opt/homebrew/bin:$PATH
+export EDITOR=nvim
+export VISUAL=nvim
 export PATH=~/bin:$PATH
+export PATH="$HOME/.local/bin:$PATH"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 export PATH="$HOME/.rd/bin:$PATH"
