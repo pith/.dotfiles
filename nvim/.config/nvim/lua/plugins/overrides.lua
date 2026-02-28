e6c280509152f7e4fa98b4f2830317d642288b2a
Pierre Thirouin <pierre.thirouin@doctolib.com>
2026-02-28 23:35:11 +0100

    change nvim theme

---
+++ nvim/.config/nvim/lua/plugins/overrides.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = { hidden = true },
+        },
+      },
+    },
+  },
+}
