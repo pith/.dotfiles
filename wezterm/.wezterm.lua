cbf5f153fa3f393793a72aeba4a5f9dea7efddbc
Pierre Thirouin <pierre.thirouin@doctolib.com>
2026-03-28 20:42:55 +0100

    feat(wezterm): switch to JetbrainsMono font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+-- config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 12
 
 config.enable_tab_bar = false
